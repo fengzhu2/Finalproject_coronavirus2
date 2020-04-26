3370bb2ecf3f8b0df8b5d4279b27513e4a48db16
Nicozhu2 <fengz2@illinois.edu>
2020-04-26 16:32:52 -0500

    with a map

--- app/src/main/java/com/example/finalprojectcoronavirus/Map.java
+++ app/src/main/java/com/example/finalprojectcoronavirus/Map.java
@@ -3,13 +3,28 @@ package com.example.finalprojectcoronavirus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Map extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        webView = findViewById(R.id.usMap);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://coronavirus.jhu.edu/us-map");
     }
 
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }
 }
