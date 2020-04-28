3cc7eca71604248d4c65ca4e123dcea3e4461c79
Nicozhu2 <fengz2@illinois.edu>
2020-04-28 08:49:10 -0500

    with a map

--- app/src/main/java/com/example/finalprojectcoronavirus/MainActivity.java
+++ app/src/main/java/com/example/finalprojectcoronavirus/MainActivity.java
@@ -9,21 +9,6 @@ import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import com.android.volley.toolbox.Volley;
-import com.android.volley.RequestQueue;
-
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 
 public class MainActivity extends AppCompatActivity {
     private Button tips;
@@ -49,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         actualTips = findViewById(R.id.button);
-        map.setOnClickListener(new View.OnClickListener() {
+        actualTips.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openMain2();
